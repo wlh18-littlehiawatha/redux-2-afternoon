d470c1f926a9754f77b177bcae13416a37709047
Jacob Hall <halljaco@yahoo.com>
2020-04-14 15:29:19 -0600

    6.1.a complete redux-2-afternoon

--- src/components/Budget/Budget.js
+++ src/components/Budget/Budget.js
@@ -1,7 +1,4 @@
 import React, { Component } from 'react';
-
-import {connect} from 'react-redux';
-
 import Background from './../shared/Background/Background'
 import Chart1 from './../shared/Chart1';
 import Chart2 from './../shared/Chart2';
@@ -10,16 +7,25 @@ import DisplayPurchases from './../shared/DisplayPurchases';
 import Loading from './../shared/Loading/Loading';
 import Nav from './../shared/Nav';
 import './Budget.css';
+import { connect } from 'react-redux'
 
 // IMPORT THE ACTION CREATOR FROM THE REDUCER FILE FOR USE BY THE CONNECT METHOD, WHICH THEN ADDS THE FUNCTION TO THE PROPS // OBJECT OF THIS COMPONENT
 import {requestUserData} from './../../ducks/userReducer'
+//IMPORT THE addPurchase AND removePurchase ACTION CREATORS
+import {requestBudgetData, addPurchase, removePurchase} from './../../ducks/budgetReducer'
 
 
 class Budget extends Component {
 
+  componentDidMount(){
+    this.props.requestUserData();
+    this.props.requestBudgetData();
+  }
+
   render() {
     // DESTRUCTURE THE LOADING PROPERTY FROM THE BUDGET OBJECT THAT WAS MAPPED TO PROPS THROUGH MAPSTATETOPROPS/CONNECT
-    const {loading } = this.props.budget;
+    // DESTRUCTURING VALUES HERE IS OPTIONAL BUT RECOMMENDED FOR CLEANER JSX
+    const {loading, purchases, budgetLimit } = this.props.budget;
     const {firstName, lastName} = this.props.user;
     return (
       <Background>
@@ -28,12 +34,13 @@ class Budget extends Component {
           <Nav firstName = {firstName} lastName = {lastName} />
           <div className='content-container'>
             <div className="purchases-container">
-              <AddPurchase />
-              <DisplayPurchases />
+{/* USE PROPS TO PASS DOWN addPurchase AND removePurchase FUNCTIONS */}
+              <AddPurchase addPurchase={this.props.addPurchase} />
+              <DisplayPurchases purchase ={purchases} removePurchase={this.props.removePurchase} />
             </div>
             <div className='chart-container'>
-              <Chart1 />
-              <Chart2 />
+              <Chart1 purchases={purchases} budgetLimit={budgetLimit} />
+              <Chart2 purchases={purchases} />
             </div>
           </div>
         </div>
@@ -59,4 +66,4 @@ function mapStateToProps(state){
 
 
 // THE CONNECT METHOD TAKES IN THE MAPSTATETOPROPS FUNCTION AND CONNECTS THIS COMPONENT TO THE REDUX STORE
-export default connect(mapStateToProps, {requestUserData})(Budget);
+export default connect(mapStateToProps, {requestUserData, requestBudgetData, addPurchase, removePurchase})(Budget);
--- src/ducks/budgetReducer.js
+++ src/ducks/budgetReducer.js
@@ -9,41 +9,72 @@ const initialState = {
    purchases: [],
    budgetLimit: null,
    loading: false
-   
+
 }
 
 //CREATE THE BASE ACTION TYPES THAT WILL BE USED
-const REQUEST_BUDGET_DATA ='REQUEST_BUDGET_DATA';
+const REQUEST_BUDGET_DATA = 'REQUEST_BUDGET_DATA';
 
+const ADD_PURCHASE = 'ADD_PURCHASE'
+const REMOVE_PURCHASE = 'REMOVE_PURCHASE'
 
 // SET UP AN ACTION CREATOR THAT RETURNS AN ACTION OBJECT WITH A TYPE AND PAYLOAD PROPERTY
 
 export const requestBudgetData = () => {
-   let data = axios.get('api/budget-data')
-   .then(res => res.data)
+   let data = axios.get('/api/budget-data').then(res => res.data)
    return {
       type: REQUEST_BUDGET_DATA,
       payload: data
    }
 }
 
+export const addPurchase = (price, description, category) => {
+  let data = axios.post('/api/budget-data/purchase', {
+    description,
+    price,
+    category
+  }).then(res => res.data);
+  return {
+    type: ADD_PURCHASE,
+    payload: data
+  }
+}
+
+export const removePurchase = (id) => {
+   let data = axios.delete(`/api/budget-data/purchase/${id}`)
+      .then(res => res.data);
+   return {
+      type: REMOVE_PURCHASE,
+      payload: data
+   }
+}
 
 
 // EXPORT DEFAULT A REDUCER FUNCTION THAT RETURNS A STATE OBJECT
-export default function reducer(state = initialState, action){
-   
+export default function reducer(state = initialState, action) {
 
-   switch(action.type) {
-      case REQUEST_BUDGET_DATA ='_PENDING':
-   
-       // IN ORDER TO KEEP VALUES PREVIOUSLY STORED ON STATE, WE SPREAD THE CURRENT STATE OBJECT
-       // INTO THE RETURNED OBJECT AND UPDATE ONLY THE VALUES IN STATE WE WANT TO CHANGE
-   
-       return {...state, loading: true}
-       case REQUEST_BUDGET_DATA + '_FULFILLED':
-          return {...state, ...action.payload, loading: false}
-          default:
-             return state;
+
+   switch (action.type) {
+      case REQUEST_BUDGET_DATA + '_PENDING':
+
+         // IN ORDER TO KEEP VALUES PREVIOUSLY STORED ON STATE, WE SPREAD THE CURRENT STATE OBJECT
+         // INTO THE RETURNED OBJECT AND UPDATE ONLY THE VALUES IN STATE WE WANT TO CHANGE
+
+         return { ...state, loading: true }
+      case REQUEST_BUDGET_DATA + '_FULFILLED':
+         return { ...state, ...action.payload, loading: false }
+
+      case ADD_PURCHASE + '_Pending':
+         return { ...state, loading: true }
+      case ADD_PURCHASE + '_FULFILLED':
+         return { ...state, purchases: action.payload, loading: false }
+      case REMOVE_PURCHASE + '_PENDING':
+         return { ...state, loading: true };
+      case removePurchase + '_FULFILLED':
+         return { ...state, loading: false, purchases: action.payload }
+
+      default:
+         return state;
    }
 
 
